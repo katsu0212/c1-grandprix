7c1684d77058c3aea78fd9632e9bed914b0e0029
katsu0212 <katsu02122728@gmail.com>
2021-12-12 02:04:33 +0900

    ユーザー管理実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '8dbde64a7a67bf07a4c832b8c2f8c4b83367ee3c21de48865ec4d6ca7eb040e9cc5e9c26c471759a55cdc7b54d6eef83cf45e60c2dc57a7b21dc9bc83bdfefb7'
