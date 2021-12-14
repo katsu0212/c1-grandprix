f3072ea7fa46495c45227638f34a146505971ff3
katsu0212 <katsu02122728@gmail.com>
2021-12-14 11:47:45 +0900

    ランキング実装

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,6 +1,5 @@
 class RanksController < ApplicationController
-  def index
-    
-    @week_post_like_ranks = Post.find(Like.group(:post_id).where(created_at: Time.current.all_week).order('count(post_id) desc').pluck(:post_id))
+  def index 
+    @month_post_like_ranks = Post.find(Like.group(:post_id).where(created_at: Time.current.all_month).order('count(post_id) desc').pluck(:post_id))
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   attachment :image
   has_many :likes, dependent: :destroy
   has_many :dislike, dependent: :destroy
-  
+
   with_options presence: true do
     validates :body
     validates :image
