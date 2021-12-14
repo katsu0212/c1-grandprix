d0b38ee61d879caa39764911848b18c564dbbca2
katsu0212 <katsu02122728@gmail.com>
2021-12-14 12:24:30 +0900

    各所編集

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,5 +1,7 @@
 class RanksController < ApplicationController
   def index 
     @month_post_like_ranks = Post.find(Like.group(:post_id).where(created_at: Time.current.all_month).order('count(post_id) desc').pluck(:post_id))
+
+    @month_post_dislike_ranks = Post.find(Dislike.group(:post_id).where(created_at: Time.current.all_month).order('count(post_id) desc').pluck(:post_id))
   end
 end
