919a68665a6747ad887ba82431153333894cebba
katsu0212 <katsu02122728@gmail.com>
2021-12-17 01:46:50 +0900

    css修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {"aaa111"}
     password_confirmation {"aaa111"}
+    profile_image_id  {'5fe9c8c01a870e9d8d21624a284967d72b93bef15614da2d5943dcc45767'}
+    
   end
 end
