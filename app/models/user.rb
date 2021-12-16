7eeec0a3a18800a1f0b3cdb014bb8d5df0c91119
katsu0212 <katsu02122728@gmail.com>
2021-12-16 19:00:54 +0900

    いいね機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :user_name
     validates :email
-    validates :password,     format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :password,     format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   end
   
 
