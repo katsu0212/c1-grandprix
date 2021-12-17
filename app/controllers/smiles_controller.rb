50dbafe09f4fbf0b2550610b220075f6285b6222
katsu0212 <katsu02122728@gmail.com>
2021-12-17 15:51:00 +0900

    smileコントローラー作成

---
+++ app/controllers/smiles_controller.rb
@@ -0,0 +1,2 @@
+class SmilesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   root to: "posts#index"
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
+  post 'smilee/:id' => 'smiles#create', as: 'create_smile'
+  delete 'smile/:id' => 'smiles#destroy', as: 'destroy_smile'
   resources :ranks, only: [:index]
   resources :users 
   resources :titles do
-    resources :likes, only: [:create, :destroy]
+    resources :smiles, only: [:create, :destroy]
   end
   resources :posts do
     resources :likes, only: [:create, :destroy]
