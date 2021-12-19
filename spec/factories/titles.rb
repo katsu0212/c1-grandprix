42a204f670ecbc7886e57ed7969734cffccd4225
katsu0212 <katsu02122728@gmail.com>
2021-12-19 22:38:54 +0900

    titleモデルテストコード

--- spec/factories/titles.rb
+++ spec/factories/titles.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :title do
-    
+    content      {'テスト'}
+    theme_id     {2}
+    association :user
   end
 end
--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Title, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @title = FactoryBot.build(:title)
+  end
+  describe '新規投稿' do
+    context '新規投稿できる場合' do
+      it 'titleとtheme_idが入力されていれば投稿できる' do
+        expect(@title).to be_valid
+      end
+    end
+    context '新規投稿できない場合' do
+      it 'titleが空では投稿できない' do
+        @title.content = nil
+        @title.valid?
+        expect(@title.errors.full_messages).to include("Content can't be blank")
+      end
+      it 'title_idが空では投稿できない' do
+        @title.theme_id = nil
+        @title.valid?
+        expect(@title.errors.full_messages).to include("Theme can't be blank")
+      end
+      it 'title_idが1では投稿できない' do
+        @title.theme_id = 1
+        @title.valid?
+        expect(@title.errors.full_messages).to include("Theme can't be blank")
+      end
+    end
+  end
 end
