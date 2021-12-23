ff846936c7da93dbb23fc36c5b116b696f82e320
katsu0212 <katsu02122728@gmail.com>
2021-12-23 19:34:55 +0900

    css編集

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -2,8 +2,8 @@ class RanksController < ApplicationController
   before_action :authenticate_user!
   
   def index 
-    @month_post_like_ranks = Post.find(Like.group(:post_id).where(created_at: Time.current.all_month).order('count(post_id) desc').limit(5).pluck(:post_id))
+    @month_post_like_ranks = Post.find(Like.group(:post_id).where(created_at: Time.current.all_month).order('count(post_id) desc').limit(4).pluck(:post_id))
 
-    @month_title_smile_ranks = Title.find(Smile.group(:title_id).where(created_at: Time.current.all_month).order('count(title_id) desc').limit(5).pluck(:title_id))
+    @month_title_smile_ranks = Title.find(Smile.group(:title_id).where(created_at: Time.current.all_month).order('count(title_id) desc').limit(4).pluck(:title_id))
   end
 end
